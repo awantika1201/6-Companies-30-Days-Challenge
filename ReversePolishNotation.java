d882b7c3b3084c74fc61c50d9e47ffeaad27ffc0
awantika1201 <awantikashinde1201@gmail.com>
2023-01-04 16:27:14 +0530

    array chnaged

--- ReversePolishNotation.java
+++ ReversePolishNotation.java
@@ -36,7 +36,7 @@ public class ReversePolishNotation {
     }
 
     public static void main(String[] args) {
-         String[] tokens =new String[] {"2","1","+","3","*"};
+         String[] tokens =new String[] {"3","1","+","3","*"};
         System.out.println(evalRPN(tokens));
 
     }

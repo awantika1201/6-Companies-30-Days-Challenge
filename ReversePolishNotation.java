0967da34d995ef53c84617c3493f6cd8296e0294
awantika1201 <awantikashinde1201@gmail.com>
2023-01-04 16:15:07 +0530

    Microsoft Q1

---
+++ ReversePolishNotation.java
@@ -0,0 +1,43 @@
+package Microsoft;
+
+import java.util.Stack;
+
+public class ReversePolishNotation {
+
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> st=new Stack<>();
+        for(String s:tokens){
+
+            if(s.equals("+")){
+                int n2=st.pop();
+                int n1=st.pop();
+                st.push(n1+n2);
+            }else if(s.equals("-")){
+                int n2=st.pop();
+                int n1=st.pop();
+                st.push(n1-n2);
+            }else if(s.equals("*")){
+                int n2=st.pop();
+                int n1=st.pop();
+                st.push(n1*n2);
+            }else if(s.equals("/")){
+                int n2=st.pop();
+                int n1=st.pop();
+                st.push(n1/n2);
+            }else{
+
+                st.push(Integer.valueOf(s));
+            }
+
+        }
+
+
+        return st.pop();
+    }
+
+    public static void main(String[] args) {
+         String[] tokens =new String[] {"2","1","+","3","*"};
+        System.out.println(evalRPN(tokens));
+
+    }
+}
